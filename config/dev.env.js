9d0f5e6b67c7412c3a3a0c2492557a224e4aaec7
chenyanfei <15873624283@163.com>
2022-05-15 18:35:59 +0800

    chore(build): 注释

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,7 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
+  // 因为只有请求devServer(http://localhost:9527)的请求才会被devServer收到，其他域名或者端口的请求devServer是收不到的
   // BASE_API: '"http://localhost:3000/api"' // 协议域名端口和路径栏不一样的请求不会被转发，而是被直接发往服务器，需要后端进行跨域处理，如cors
   // BASE_API: '"http://localhost:9527/api"' // 如果带上协议域名端口等信息，需要和当前页面路径栏的保持一致
   // BASE_API: '"/"' // 以'/'开头的请求也可以被转发 'http://localhost:9527/'
